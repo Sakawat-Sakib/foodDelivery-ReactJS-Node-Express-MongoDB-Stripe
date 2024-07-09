f800aa983246746eec1b41223f5593290b161cc2
sakawat-sakib <mdsakawatsakib@gmail.com>
2024-07-09 11:34:19 +0600

    Home page structure created

---
+++ frontEnd/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='header-contents'>
+            <h2>Order your favourite food here</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quo, quasi provident laudantium magni explicabo dignissimos quam nemo ducimus quibusdam vero suscipit accusantium delectus, nobis, mollitia aperiam libero repudiandae! Mollitia.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- frontEnd/src/components/Navbar/Navbar.jsx
+++ frontEnd/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
-
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
+    const [menu,setMenu] = useState("home");
+
   return (
     <div className='navBar'>
-        
+        <img src={assets.logo} className='logo' alt="" />
+        <ul className='navbar-menu'>
+            
+            <Link to='/' onClick={()=>setMenu("home")} className={menu==='home'?'active':''}>Home</Link>
+            <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==='menu'?'active':''}>Menu</a>
+            <a href='#app-download' onClick={()=>setMenu("mobile")} className={menu==='mobile'?'active':''}>Mobile-app</a>
+            <a href='#footer' onClick={()=>setMenu("contact")} className={menu==='contact'?'active':''}>Contact Us</a>
+        </ul>
+        <div className='navbar-right'>
+            <img src={assets.search_icon} alt="" />
+            <div className='navbar-search-icon'>
+                <img src={assets.basket_icon} alt="" />
+                <div className='dot'></div>
+            </div>
+            <button>Sign In</button>
+        </div>
     </div>
   )
 }
